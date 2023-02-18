a7d7c1d14b331f1c4eea2de66270b9b6f9a41326
Dominik Gumienny <dominik.gumienny00@gmail.com>
2023-02-18 12:39:52 +0100

    snake

--- game.py
+++ game.py
@@ -4,6 +4,17 @@ import random
 from pygame.math import Vector2
 
 
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            block_rect = pygame.Rect(
+                int(block.x * cell_size), int(block.y * cell_size), cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+
 class Fruit:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1)
@@ -25,6 +36,7 @@ screen = pygame.display.set_mode(
 clock = pygame.time.Clock()
 
 fruit = Fruit()
+snake = Snake()
 
 while True:
     for event in pygame.event.get():
@@ -33,5 +45,6 @@ while True:
             sys.exit()
     screen.fill((175, 215, 70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)

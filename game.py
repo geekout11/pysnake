de26302d1f5a3f042423c20fd29123b6226c3691
Dominik Gumienny <dominik.gumienny00@gmail.com>
2023-02-18 13:27:04 +0100

    snake extending after eating a fruit

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Snake:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
         self.direction = Vector2(1, 0)
+        self.new_block = False
 
     def draw_snake(self):
         for block in self.body:
@@ -16,9 +17,18 @@ class Snake:
             pygame.draw.rect(screen, (183, 111, 122), block_rect)
 
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + self.direction)
-        self.body = body_copy[:]
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True
 
 
 class Fruit:
@@ -37,6 +47,7 @@ class Fruit:
         self.y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.x, self.y)
 
+
 class Main:
     def __init__(self):
         self.snake = Snake()
@@ -53,6 +64,7 @@ class Main:
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
+            self.snake.add_block()
 
 
 pygame.init()

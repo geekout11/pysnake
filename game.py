e681bc9c86276b10f5df170bff1395c35ce87cac
Dominik Gumienny <dominik.gumienny00@gmail.com>
2023-02-19 11:46:39 +0100

    improved the look of the scoreboard

--- game.py
+++ game.py
@@ -186,9 +186,15 @@ class Main:
         score_x = int(cell_size * cell_number - 60)
         score_y = int(cell_size * cell_number - 40)
         score_rect = score_surface.get_rect(center=(score_x, score_y))
-        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+        apple_rect = apple.get_rect(
+            midright=(score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top,
+                              apple_rect.width + score_rect.width, apple_rect.height)
+
+        pygame.draw.rect(screen, (167, 209, 61), bg_rect)
         screen.blit(score_surface, score_rect)
         screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
 
 pygame.init()

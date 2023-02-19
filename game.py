2e25a674ee3c16756ed61b9aa7de7623755eb9ac
Dominik Gumienny <dominik.gumienny00@gmail.com>
2023-02-19 10:51:30 +0100

    update tail graphics, update vertical body graphics

--- game.py
+++ game.py
@@ -44,6 +44,7 @@ class Snake:
 
     def draw_snake(self):
         self.update_head_graphics()
+        self.update_tail_graphics()
 
         for index, block in enumerate(self.body):
             x_pos = int(block.x * cell_size)
@@ -52,8 +53,14 @@ class Snake:
 
             if index == 0:
                 screen.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
             else:
-                pygame.draw.rect(screen, (150, 100, 100), block_rect)
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
 
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
@@ -65,6 +72,17 @@ class Snake:
             self.head = self.head_up
         elif head_relation == Vector2(0, -1):
             self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1):
+            self.tail = self.tail_down
 
     def move_snake(self):
         if self.new_block == True:
